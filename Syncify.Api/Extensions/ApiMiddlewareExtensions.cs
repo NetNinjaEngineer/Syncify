f1c5d5b196d2da3798aaddb0c1a254d25414fa8c
NetNinjaEngineer <memo5260287@outlook.com>
2024-12-17 03:11:23 -0800

    Refactor program.cs

---
+++ Syncify.Api/Extensions/ApiMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Syncify.Api.Middleware;
+
+namespace Syncify.Api.Extensions;
+
+public static class ApiMiddlewareExtensions
+{
+    public static IApplicationBuilder UseApiMiddlewares(this WebApplication app)
+    {
+        app.UseMiddleware<MigrateDatabaseMiddleware>();
+        app.UseMiddleware<JwtValidationMiddleware>();
+
+        return app;
+    }
+}
--- Syncify.Api/Program.cs
+++ Syncify.Api/Program.cs
@@ -1,58 +1,26 @@
-using Microsoft.AspNetCore.Http.Features;
+using Syncify.Api;
 using Syncify.Api.Extensions;
-using Syncify.Api.Filters;
-using Syncify.Api.Middleware;
 using Syncify.Application;
-using Syncify.Application.Hubs;
 using Syncify.Infrastructure;
 using Syncify.Persistence;
 using Syncify.Services;
-using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers()
-    .AddJsonOptions(opt => opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddInfrastructureDependencies(builder.Configuration)
     .AddApplicationDependencies(builder.Configuration)
     .AddServicesDependencies(builder.Configuration)
-    .AddPersistenceDependencies(builder.Configuration);
-
-builder.Services.AddGlobalExceptionHandler();
-
-builder.Services.AddSwaggerDocumentation();
-
-builder.Services.AddSignalR(options => options.EnableDetailedErrors = true);
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
-    serverOptions.Limits.MaxRequestBodySize = Convert.ToInt64(builder.Configuration["FormOptionsSize"]));
-
-builder.Services.Configure<IISServerOptions>(options =>
-    options.MaxRequestBodySize = Convert.ToInt64(builder.Configuration["FormOptionsSize"]));
-
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MultipartBodyLengthLimit = Convert.ToInt64(builder.Configuration["FormOptionsSize"]);
-    options.ValueLengthLimit = int.MaxValue;
-    options.MemoryBufferThreshold = int.MaxValue;
-});
-
-builder.Services.AddScoped<MigrateDatabaseMiddleware>();
-
-builder.Services.AddSingleton<ApiKeyFilter>();
-
-builder.Services.AddSingleton<GuardFilter>();
-
+    .AddPersistenceDependencies(builder.Configuration)
+    .AddApiDependencies(
+        builder.Configuration,
+        builder.WebHost);
 
 var app = builder.Build();
 
 app.UseSwaggerDocumentation();
 
-app.UseMiddleware<MigrateDatabaseMiddleware>();
-
-app.UseMiddleware<JwtValidationMiddleware>();
+app.UseApiMiddlewares();
 
 app.UseStaticFiles();
 
@@ -66,10 +34,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapHub<NotificationHub>("/hubs/notifications");
-
-app.MapHub<MessageHub>("/hubs/messages");
-
-app.MapHub<FriendRequestHub>("/hubs/friendRequests");
+app.UseHubs();
 
 app.Run();
