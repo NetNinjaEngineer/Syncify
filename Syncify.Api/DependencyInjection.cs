f1c251530c3422fd311de104c803df933f0b2a50
NetNinjaEngineer <memo5260287@outlook.com>
2024-12-17 03:11:23 -0800

    Refactor program.cs

---
+++ Syncify.Api/DependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Syncify.Api.Extensions;
+using Syncify.Api.Filters;
+using Syncify.Api.Middleware;
+
+namespace Syncify.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiDependencies(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        ConfigureWebHostBuilder webHostBuilder)
+    {
+        services.AddGlobalExceptionHandler();
+
+        services.AddSwaggerDocumentation();
+
+        services.AddSignalR(options => options.EnableDetailedErrors = true);
+
+        webHostBuilder.ConfigureKestrel(serverOptions =>
+            serverOptions.Limits.MaxRequestBodySize = Convert.ToInt64(configuration["FormOptionsSize"]));
+
+        services.Configure<IISServerOptions>(options =>
+            options.MaxRequestBodySize = Convert.ToInt64(configuration["FormOptionsSize"]));
+
+        services.Configure<FormOptions>(options =>
+        {
+            options.MultipartBodyLengthLimit = Convert.ToInt64(configuration["FormOptionsSize"]);
+            options.ValueLengthLimit = int.MaxValue;
+            options.MemoryBufferThreshold = int.MaxValue;
+        });
+
+        services.AddScoped<MigrateDatabaseMiddleware>();
+
+        services.AddSingleton<ApiKeyFilter>();
+
+        services.AddSingleton<GuardFilter>();
+
+        return services;
+    }
+}
---
+++ Syncify.Api/Extensions/SignalRHubExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Syncify.Application.Hubs;
+
+namespace Syncify.Api.Extensions;
+
+public static class SignalRHubExtensions
+{
+    public static IApplicationBuilder UseHubs(this WebApplication app)
+    {
+        app.MapHub<NotificationHub>("/hubs/notifications");
+
+        app.MapHub<MessageHub>("/hubs/messages");
+
+        app.MapHub<FriendRequestHub>("/hubs/friendRequests");
+
+        return app;
+    }
+}
